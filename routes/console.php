53e83707ebf66ccfa936bed297e275fe044bc2b4
johhanedukin <johhanedukin@gmail.com>
2019-06-07 21:50:28 -0500

    mi primer copmmit2

--- routes/console.php
+++ routes/console.php
@@ -16,3 +16,9 @@ use Illuminate\Foundation\Inspiring;
 Artisan::command('inspire', function () {
     $this->comment(Inspiring::quote());
 })->describe('Display an inspiring quote');
+
+
+
+Artisan::command('inspire', function () {
+    $this->comment(Inspiring::quote());
+})->describe('Display an hola    inspiring quote');
